1072772fc7752fe4ba6e50b3a09f574879df104e
velvetsecretco <andresmontoya217@gmail.com>
2026-09-18 11:27:35 -0500

    Update disfraces.js

--- js/disfraces.js
+++ js/disfraces.js
@@ -461,4 +461,512 @@ window.listaProductos = [
         "../img/Productos/Disfraces/21.jpg"
     ]
 },
+    {
+    nombre: "Disfraz de Enfermera",
+
+    referencia: "1272",
+
+    codigo: "4494",
+
+    precio: "$90.000",
+
+    categoria: "enfermera",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Única"],
+
+    descripcion:
+        "Disfraz temático de enfermera, diseñado para crear un look llamativo y diferente en ocasiones especiales.",
+
+    caracteristicas: [
+        "Temática de enfermera.",
+        "Talla única.",
+        "Diseño temático."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/22.jpg"
+    ]
+},
+ {
+    nombre: "Disfraz de Coneja",
+
+    referencia: "1442",
+
+    codigo: "4493",
+
+    precio: "$86.000",
+
+    categoria: "coneja",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Única"],
+
+    descripcion:
+        "Disfraz temático de coneja, diseñado para crear un look llamativo y diferente en ocasiones especiales.",
+
+    caracteristicas: [
+        "Temática de coneja.",
+        "Talla única.",
+        "Diseño temático."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/23.jpg"
+    ]
+},   
+    {
+    nombre: "Disfraz de Ángel Blanco",
+
+    referencia: "1172",
+
+    codigo: "4489",
+
+    precio: "$70.000",
+
+    categoria: "angel",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Única"],
+
+    descripcion:
+        "Disfraz temático de ángel en color blanco, diseñado para crear un look llamativo y diferente en ocasiones especiales.",
+
+    caracteristicas: [
+        "Temática de ángel.",
+        "Color blanco.",
+        "Talla única.",
+        "Diseño temático."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/24.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Black Minnie",
+
+    referencia: "",
+
+    codigo: "4483",
+
+    precio: "$166.000",
+
+    categoria: "personajes",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Black Minnie con diseño llamativo y sofisticado, pensado para resaltar la silueta y crear un look diferente para ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño inspirado en Minnie.",
+        "Estilo Black Minnie.",
+        "Diseño llamativo y sofisticado.",
+        "Detalles que resaltan la silueta.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/25.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Aphrodite",
+
+    referencia: "",
+
+    codigo: "4481",
+
+    precio: "$199.000",
+
+    categoria: "fantasia",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Aphrodite con diseño llamativo y sofisticado, pensado para resaltar la silueta y crear un look especial inspirado en la fantasía.",
+
+    caracteristicas: [
+        "Diseño Aphrodite.",
+        "Estilo llamativo y sofisticado.",
+        "Detalles que resaltan la silueta.",
+        "Diseñado para ocasiones especiales.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/26.jpg",
+        "../img/Productos/Disfraces/27.jpg",
+        "../img/Productos/Disfraces/28.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Blancanieves Fairytale",
+
+    referencia: "",
+
+    codigo: "4477",
+
+    precio: "$162.000",
+
+    categoria: "personajes",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Blancanieves Fairytale con diseño llamativo y sofisticado, inspirado en el clásico personaje de fantasía y pensado para crear un look especial.",
+
+    caracteristicas: [
+        "Diseño inspirado en Blancanieves.",
+        "Estilo Fairytale.",
+        "Diseño llamativo y sofisticado.",
+        "Detalles que resaltan la silueta.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/29.jpg",
+        "../img/Productos/Disfraces/30.jpg",
+        "../img/Productos/Disfraces/31.jpg",
+        "../img/Productos/Disfraces/32.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Pirata Lola",
+
+    referencia: "",
+
+    codigo: "4478",
+
+    precio: "$193.000",
+
+    categoria: "fantasia",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Pirata Lola con diseño llamativo y sofisticado, inspirado en una temática pirata y pensado para crear un look especial y diferente.",
+
+    caracteristicas: [
+        "Temática de pirata.",
+        "Diseño Pirata Lola.",
+        "Estilo llamativo y sofisticado.",
+        "Detalles que resaltan la silueta.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/33.jpg",
+        "../img/Productos/Disfraces/34.jpg",
+        "../img/Productos/Disfraces/35.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Night Queen",
+
+    referencia: "",
+
+    codigo: "4479",
+
+    precio: "$279.000",
+
+    categoria: "fantasia",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Night Queen con diseño llamativo y sofisticado, pensado para resaltar la silueta y crear un look elegante y diferente para ocasiones especiales.",
+
+    caracteristicas: [
+        "Diseño Night Queen.",
+        "Estilo llamativo y sofisticado.",
+        "Detalles que resaltan la silueta.",
+        "Diseñado para ocasiones especiales.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/36.jpg",
+        "../img/Productos/Disfraces/37.jpg",
+        "../img/Productos/Disfraces/38.jpg",
+        "../img/Productos/Disfraces/39.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Arabian Nights",
+
+    referencia: "",
+
+    codigo: "4480",
+
+    precio: "$213.000",
+
+    categoria: "fantasia",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Arabian Nights con diseño llamativo y sofisticado, inspirado en una temática árabe de fantasía y pensado para crear un look especial y diferente.",
+
+    caracteristicas: [
+        "Temática Arabian Nights.",
+        "Estilo inspirado en la fantasía árabe.",
+        "Diseño llamativo y sofisticado.",
+        "Detalles que resaltan la silueta.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/40.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Marinera Sexy",
+
+    referencia: "4419",
+
+    codigo: "4419",
+
+    precio: "$115.000",
+
+    categoria: "marinera",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["S/M", "M/L"],
+
+    descripcion:
+        "Disfraz Marinera Sexy Lerot inspirado en el clásico estilo náutico, con una combinación de blanco y azul acompañada de detalles rojos.",
+
+    caracteristicas: [
+        "Estilo náutico.",
+        "Combinación de colores blanco y azul.",
+        "Detalles en color rojo.",
+        "Diseño llamativo.",
+        "Disponible en tallas S/M y M/L."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/41.jpg",
+        "../img/Productos/Disfraces/42.jpg",
+        "../img/Productos/Disfraces/43.jpg",
+        "../img/Productos/Disfraces/44.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Vilma Picapiedra",
+
+    referencia: "4418",
+
+    codigo: "4418",
+
+    precio: "$103.000",
+
+    categoria: "personajes",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["S/M", "M/L"],
+
+    descripcion:
+        "Disfraz Vilma Picapiedra Lerot inspirado en el clásico personaje, con vestido naranja y estampado negro de estilo prehistórico.",
+
+    caracteristicas: [
+        "Diseño inspirado en Vilma Picapiedra.",
+        "Vestido en color naranja.",
+        "Estampado negro de estilo prehistórico.",
+        "Escote profundo.",
+        "Amarre frontal.",
+        "Disponible en tallas S/M y M/L."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/45.jpg",
+        "../img/Productos/Disfraces/46.jpg",
+        "../img/Productos/Disfraces/47.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Vaquita Sexy – Lerot",
+
+    referencia: "",
+
+    codigo: "4417",
+
+    precio: "$99.000",
+
+    categoria: "fantasia",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["S/M", "M/L"],
+
+    descripcion:
+        "Disfraz Vaquita Sexy Lerot con estampado blanco y negro inspirado en animal print, diseñado para crear un look divertido, coqueto y diferente.",
+
+    caracteristicas: [
+        "Diseño inspirado en vaquita.",
+        "Estampado blanco y negro.",
+        "Estilo animal print.",
+        "Diseño llamativo y coqueto.",
+        "Disponible en tallas S/M y M/L."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/48.jpg",
+        "../img/Productos/Disfraces/49.jpg",
+        "../img/Productos/Disfraces/50.jpg",
+        "../img/Productos/Disfraces/51.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Pretty Angel",
+
+    referencia: "",
+
+    codigo: "4546",
+
+    precio: "$265.000",
+
+    categoria: "fantasia",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Pretty Angel con diseño inspirado en una temática angelical. Su bralette puede utilizarse también como prenda exterior para combinarlo en diferentes looks.",
+
+    caracteristicas: [
+        "Diseño inspirado en ángel.",
+        "Incluye bralette que puede utilizarse como prenda exterior.",
+        "Diseño versátil para combinar con otros looks.",
+        "Estilo llamativo y sofisticado.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/52.jpg",
+        "../img/Productos/Disfraces/53.jpg",
+        "../img/Productos/Disfraces/54.jpg",
+        "../img/Productos/Disfraces/55.jpg",
+        "../img/Productos/Disfraces/56.jpg",
+        "../img/Productos/Disfraces/57.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Gatubela",
+
+    referencia: "",
+
+    codigo: "4442",
+
+    precio: "$241.000",
+
+    categoria: "personajes",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Gatubela con diseño coqueto y llamativo. Su enterizo puede utilizarse también como prenda exterior para crear diferentes looks.",
+
+    caracteristicas: [
+        "Diseño inspirado en Gatubela.",
+        "Incluye enterizo.",
+        "Incluye antifaz.",
+        "El enterizo puede utilizarse como prenda exterior.",
+        "Diseño versátil para combinar con otros looks.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/58.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz de Alicia",
+
+    referencia: "",
+
+    codigo: "4439",
+
+    precio: "$213.000",
+
+    categoria: "personajes",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: ["Única"],
+
+    descripcion:
+        "Disfraz inspirado en la clásica historia de Alicia, con un diseño coqueto que combina vestido con short, delantal y diadema con moño.",
+
+    caracteristicas: [
+        "Incluye diadema con moño.",
+        "Incluye vestido con short.",
+        "Incluye delantal.",
+        "Material: 85% poliéster y 15% elastano.",
+        "Talla única.",
+        "Producto bajo pedido.",
+        "Entrega aproximada en 2 días hábiles.",
+        "Requiere pago anticipado."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/59.jpg",
+        "../img/Productos/Disfraces/60.jpg",
+        "../img/Productos/Disfraces/61.jpg"
+    ]
+},
+    {
+    nombre: "Disfraz Harley Pink",
+
+    referencia: "",
+
+    codigo: "4445",
+
+    precio: "$213.000",
+
+    categoria: "personajes",
+
+    nombreCategoria: "Disfraces",
+
+    tallas: [],
+
+    descripcion:
+        "Disfraz Harley Pink con diseño en tonos fucsia y negro, inspirado en un estilo atrevido y juguetón.",
+
+    caracteristicas: [
+        "Diseño Harley Pink.",
+        "Combinación de colores fucsia y negro.",
+        "Incluye gargantilla.",
+        "Incluye body.",
+        "Incluye guantes.",
+        "Incluye tutú.",
+        "Consultar disponibilidad de talla."
+    ],
+
+    imagenes: [
+        "../img/Productos/Disfraces/62.jpg",
+        "../img/Productos/Disfraces/63.jpg"
+    ]
+},
 ];
